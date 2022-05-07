aedcf71980ffb4d455113543184bae39daab241a
alberdinamariya <mariyaalberdina1980@gmail.com>
2022-05-07 12:08:16 +0300

    afternoon 7 may

--- Zoo/Accountings/models.py
+++ Zoo/Accountings/models.py
@@ -6,9 +6,22 @@ class Animal(models.Model):
     name = models.CharField(max_length=45)
     type = models.CharField(max_length=10)
     predators = models.CharField(max_length=3)
+    meal = models.ForeignKey('Meal', on_delete=models.PROTECT, null=True)
 
     class Meta:
         ordering = ["-name"]
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
+
+class Meal(models.Model):
+    name = models.CharField(max_length=45)
+    cur = models.IntegerField()
+    measure = models.CharField(max_length=3)
+    type = models.CharField(max_length=10)
+    normofconsumption = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
