9f27624db13a7ffaf5dbd87225e9671fc9d9fd9b
alberdinamariya <mariyaalberdina1980@gmail.com>
2022-05-09 18:31:40 +0300

    8 may

--- Zoo/Accountings/admin.py
+++ Zoo/Accountings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'cur', 'measure', 'type', 'normofconsumption')
+
+admin.site.register(Animal)
+
+
+
+
+admin.site.register(Meal)
